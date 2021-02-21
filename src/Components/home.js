3d7a722f204fac8f7efdf7a9245f794f167318b6
HAKJOON0223 <2hakjoon@gmail.com>
2021-02-21 14:28:33 +0900

    Socket io setting is done

---
+++ src/Components/home.js
@@ -0,0 +1,5 @@
+function App() {
+    return "this is my frontend"
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './Components/home';
+import socketio from 'socket.io-client';
+
+const socket = socketio.connect('http://localhost:4000');
+
+(() => {
+  socket.emit('init', { name: 'bella' });
+  socket.on('welcome', (msg) => {
+    console.log(msg);
+  });
+  
+})();
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +20,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
