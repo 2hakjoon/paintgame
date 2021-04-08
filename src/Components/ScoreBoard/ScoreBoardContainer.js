1f5c491665010c071ab3de49bea64bf6e3d4fae5
HAKJOON0223 <2hakjoon@gmail.com>
2021-04-08 22:09:04 +0900

    test

--- src/Components/ScoreBoard/ScoreBoardContainer.js
+++ src/Components/ScoreBoard/ScoreBoardContainer.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { enableCanvas, showControls } from "../../Routes/GameRoom/CanvasController";
 import commends from "../../Socket/Commends";
 import { getSocket } from "../../Socket/Socket";
 import ScoreBoardPresentser from "./ScoreBoardPresentser"
@@ -11,6 +12,10 @@ export default () =>{
         if (setListener === false){
             getSocket().on(commends.userList, data=>{
                 setUserList(userList.concat(data));
+                if(userList.length === 1){
+                    enableCanvas();
+                    showControls();
+                }
             });
         }
         setListener = true;
